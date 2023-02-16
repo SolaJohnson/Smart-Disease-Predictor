d47ebc703cc3364d1d10f8de8e3bee783d148836
Johnson Amodu <amodu.akinsola@gmail.com>
2023-02-16 21:57:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import os
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-from PIL import Image
-import tensorflow as tf
 
 app = Flask(__name__)
 
@@ -94,14 +92,6 @@ def kidneyPage():
 def liverPage():
     return render_template('liver.html')
 
-@app.route("/malaria", methods=['GET', 'POST'])
-def malariaPage():
-    return render_template('malaria.html')
-
-@app.route("/pneumonia", methods=['GET', 'POST'])
-def pneumoniaPage():
-    return render_template('pneumonia.html')
-
 @app.route("/predict", methods = ['POST', 'GET'])
 def predictPage():
     try:
@@ -121,44 +111,6 @@ def predictPage():
         return render_template("home.html", message=message)
 
     return render_template('predict.html', pred=pred)
-
-@app.route("/malariapredict", methods = ['POST', 'GET'])
-def malariapredictPage():
-    if request.method == 'POST':
-        try:
-            img = Image.open(request.files['image'])
-            img.save("uploads/image.jpg")
-            img_path = os.path.join(os.path.dirname(__file__), 'uploads/image.jpg')
-            os.path.isfile(img_path)
-            img = tf.keras.utils.load_img(img_path, target_size=(128, 128))
-            img = tf.keras.utils.img_to_array(img)
-            img = np.expand_dims(img, axis=0)
-
-            model = tf.keras.models.load_model("models/malaria.h5")
-            pred = np.argmax(model.predict(img))
-        except:
-            message = "Please upload an image"
-            return render_template('malaria.html', message=message)
-    return render_template('malaria_predict.html', pred=pred)
-
-@app.route("/pneumoniapredict", methods = ['POST', 'GET'])
-def pneumoniapredictPage():
-    if request.method == 'POST':
-        try:
-            img = Image.open(request.files['image']).convert('L')
-            img.save("uploads/image.jpg")
-            img_path = os.path.join(os.path.dirname(__file__), 'uploads/image.jpg')
-            os.path.isfile(img_path)
-            img = tf.keras.utils.load_img(img_path, target_size=(128, 128))
-            img = tf.keras.utils.img_to_array(img)
-            img = np.expand_dims(img, axis=0)
-
-            model = tf.keras.models.load_model("models/pneumonia.h5")
-            pred = np.argmax(model.predict(img))
-        except:
-            message = "Please upload an image"
-            return render_template('pneumonia.html', message=message)
-    return render_template('pneumonia_predict.html', pred=pred)
 
 if __name__ == '__main__':
     app.run(debug = True)
